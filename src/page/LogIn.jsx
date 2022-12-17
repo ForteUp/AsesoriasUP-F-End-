50091c47fd82213cd132d04531cb4b60eac66b28
Omar Vidaña Rodríguez <107644289+OmVidana@users.noreply.github.com>
2022-12-17 02:11:39 -0600

    Google Material Icons Added

--- src/page/LogIn.jsx
+++ src/page/LogIn.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import './LogIn.scss';
-import Tree from '../assets/tree.png';
+import React from "react";
+import "./LogIn.scss";
+import Tree from "../assets/tree.png";
 
 function LogIn() {
-  return (
-    <div className='login-page'>
-        <h1>Sexopene</h1>
-        <p>Hola Pompis</p>
-        <div className='login-container'>
-            <img src={Tree} alt='tree'/>
+    return (
+        <div className="login-page">
+            <div className="login-container">
+                <img src={Tree} alt="tree" />
+            </div>
         </div>
-    </div>
-  )
-
+    );
 }
 
-export default LogIn
+export default LogIn;
