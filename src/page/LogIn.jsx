c82137d1042a80acc6804f1302950e5ff86a0041
Omar Vidaña Rodríguez <107644289+OmVidana@users.noreply.github.com>
2022-12-17 05:20:36 -0600

    Base Login Design (Only Desktop)
    Missing Design for Inputs and Button. I just placed some placeholders cause I wana make those as components in order to use them globally if needed.

--- src/page/LogIn.jsx
+++ src/page/LogIn.jsx
@@ -5,6 +5,24 @@ import Tree from "../assets/tree.png";
 function LogIn() {
     return (
         <div className="login-page">
+            <form className="login-form">
+                <h1>ASESORÍAS UNIVERSITARIAS UP</h1>
+                <h2>Inicia Sesión</h2>
+                <p>Ingresa con tu correo institucional de la Universidad Panamericana</p>
+                <input
+                        type="mail"
+                        placeholder="Correo container temporal"
+                        name="email"
+                        required
+                    />
+                    <input
+                        type="password"
+                        placeholder="Contraseña container temporal"
+                        name="pwd"
+                        required
+                    />
+                    <button className="login-button" type="submit">Ingresar Temporal</button>
+            </form>
             <div className="login-container">
                 <img src={Tree} alt="tree" />
             </div>
