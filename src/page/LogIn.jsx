a03018e554a43d42e107c189b9b0401c2893b10a
Omar Vidaña Rodríguez <107644289+OmVidana@users.noreply.github.com>
2022-12-19 19:52:55 -0600

    New Component Added - InputField
    The design is halfway done but it suppose to change depending where you are using it.
    
    In the LogIn sass I will change the design to fit with everything.

--- src/page/LogIn.jsx
+++ src/page/LogIn.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import InputField from "../components/Global/InputField";
 import "./LogIn.scss";
 import Tree from "../assets/tree.png";
 
@@ -6,22 +7,30 @@ function LogIn() {
     return (
         <div className="login-page">
             <form className="login-form">
-                <h1>ASESORÍAS UNIVERSITARIAS UP</h1>
-                <h2>Inicia Sesión</h2>
-                <p>Ingresa con tu correo institucional de la Universidad Panamericana</p>
-                <input
-                        type="mail"
-                        placeholder="Correo container temporal"
-                        name="email"
-                        required
-                    />
-                    <input
-                        type="password"
-                        placeholder="Contraseña container temporal"
-                        name="pwd"
-                        required
-                    />
-                    <button className="login-button" type="submit">Ingresar Temporal</button>
+                <h1 className="login-title">ASESORÍAS UNIVERSITARIAS UP</h1>
+                <h2 className="login-subtitle">Inicia Sesión</h2>
+                <p className="login-text">
+                    Ingresa con tu correo institucional de la Universidad
+                    Panamericana
+                </p>
+                <InputField
+                    className="login-mail"
+                    textDisplay="Correo Institucional"
+                    type="mail"
+                    placeholder=""
+                    value="temporal@up.edu.mx"
+                />
+                <InputField
+                    className="login-pwd"
+                    textDisplay="Contraseña"
+                    type="password"
+                    placeholder=""
+                    value="temporal"
+                />
+                
+                <button className="login-button" type="submit">
+                    Ingresar Temporal
+                </button>
             </form>
             <div className="login-container">
                 <img src={Tree} alt="tree" />
