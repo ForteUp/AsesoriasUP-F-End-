bc4fbb30d2876046280a364f156023b0ef42d5a4
DanielPrado0245455 <0245455@up.edu.mx>
2023-01-19 01:10:33 -0600

    Modify scss of the navbar and add image logo

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,7 @@ function App() {
                         <Route
                             path="/agendar"
                             element={
-                                <PrivateAsesorRoute>
                                     <AgendarCita />
-                                </PrivateAsesorRoute>
                             }
                         />
                         <Route path="/login" element={<LogIn />} />
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import "./NavBar.scss";
 import ImgProfile from "../../assets/VectorProfile.svg";
+import ImgLogo from "../../assets/roble-removebg-preview 1roble.png";
 
 function NavBar() {
   return (
     <nav className='NavBar-body'>
+        <div className='NavBar-Logo' itemType='button'>
+            <img src={ImgLogo} className="NavBar-Img"/>
+            <p className='NavBar-text1'>Asesorias</p>
+            <p className='NavBar-text2'>Universidad</p>
+        </div>
         <div className='NavBar-Menu'>
             <div className='opcionAgendar'>
                 <p className='opcionAgendar-text'>Agendar</p>
             </div>
-            <div className='opcionRegistrar'>
-                <p className='opcionRegistrar-text'>Registrar</p>
+            <div className='opcionVerCitas'>
+                <p className='opcionVerCitas-text'>Ver Citas</p>
             </div>
             <div className='opcionMisEventos'>
                 <p className='opcionMisEventos-text'>Mis Eventos</p>
             </div>
             <div className='opcionProfile'>
-                <img src={ImgProfile}  className='opcionProfile-text'/>
+                <div className='opcionProfile-account'>
+                    <img src={ImgProfile}  className='opcionProfile-img'/>
+                </div>
             </div>
         </div>
     </nav>
