7c9a2a61670fb12bbda05ab7edd27c4d9c893681
DanielPrado0245455 <0245455@up.edu.mx>
2023-01-18 21:26:08 -0600

    Add Image profile and modify navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import "./NavBar.scss";
+import ImgProfile from "../../assets/VectorProfile.svg";
 
 function NavBar() {
   return (
     <nav className='NavBar-body'>
-
+        <div className='NavBar-Menu'>
+            <div className='opcionAgendar'>
+                <p className='opcionAgendar-text'>Agendar</p>
+            </div>
+            <div className='opcionRegistrar'>
+                <p className='opcionRegistrar-text'>Registrar</p>
+            </div>
+            <div className='opcionMisEventos'>
+                <p className='opcionMisEventos-text'>Mis Eventos</p>
+            </div>
+            <div className='opcionProfile'>
+                <img src={ImgProfile}  className='opcionProfile-text'/>
+            </div>
+        </div>
     </nav>
   )
 }
