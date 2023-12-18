0e016702002ee63902586b99a63915eda1278e4f
DanielPrado0245455 <0245455@up.edu.mx>
2023-12-18 16:59:05 -0600

    Cambios en login

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,13 +3,9 @@ import "./NavBar.scss";
 import ImgProfile from "../../assets/VectorProfile.svg";
 import ImgLogo from "../../assets/roble-removebg-preview 1roble.png";
 import { Link } from "react-router-dom";
-import ImgMenu from "../../assets/menu.svg";
-
 
 function NavBar() {
   return (
-    <>
-    <input type="checkbox" id="check" />
     <nav className='NavBar-body'>
         <div className='NavBar-Logo' itemType='button'>
             <img src={ImgLogo} className="img" alt=''/>
@@ -17,25 +13,8 @@ function NavBar() {
         </div>
         
         <div className='NavBar-Menu'>
-            <Link to="/agendar" className='opcion' inputMode=''>
-                <p className='text'>Agendar</p>
-            </Link>
-            <Link to="/citas"className='opcion'>
-                <p className='text'>Ver Citas</p>
-            </Link>
-            <div className='opcion'>
-                <p className='text'>Mis Eventos</p>
-            </div>       
-            <div className='opcion'>
-                <img src={ImgProfile}  className='imgProfile' alt=''/>
-            </div>
         </div>     
-        <label htmlFor="check" className='checkbtn'>                     
-            <img src={ImgMenu}  className='img' alt=''/>
-        </label>
     </nav>
-    </>
-    
   )
 }
 
--- src/page/LogIn.jsx
+++ src/page/LogIn.jsx
@@ -14,32 +14,32 @@ function LogIn() {
         <div className="login-page">
             <form className="login-form" onSubmit={loginUser}>
             {/*<form className="login-form">*/}
-                <h1 className="login-title">ASESORÍAS UNIVERSITARIAS UP</h1>
-                <h2 className="login-subtitle">Inicia Sesión</h2>
-                <p className="login-text">
-                    Ingresa con tu correo institucional de la Universidad
-                    Panamericana
-                </p>
-                <InputField
-                    className="login-mail"
-                    textDisplay="Correo Institucional"
-                    type="mail"
-                    placeholder=""
-                    // value="0244198@up.edu.mx"
-                />
-                <InputField
-                    className="login-pwd"
-                    textDisplay="Contraseña"
-                    type="password"
-                    placeholder=""
-                    // value="84079866"
-                />
-                <Button 
-                    className="login-button"
-                    type="submit"
-                    buttonContent="Ingresar"
-                />
-            </form>
+            <h1 className="login-title">ASESORÍAS Universitarias UP</h1>
+            <h2 className="login-subtitle">Inicia Sesión</h2>
+            <p className="login-text">
+                Ingresa con tu correo institucional de la Universidad
+                Panamericana
+            </p>
+            <InputField
+                className="login-mail"
+                textDisplay="Correo Institucional"
+                type="mail"
+                placeholder=""
+                // value="0244198@up.edu.mx"
+            />
+            <InputField
+                className="login-pwd"
+                textDisplay="Contraseña"
+                type="password"
+                placeholder=""
+                // value="84079866"
+            />
+            <Button 
+                className="login-button"
+                type="submit"
+                buttonContent="Ingresar"
+            />
+            </form>         
             <div className="login-container">
                 <img src={Tree} alt="tree" />
             </div>
